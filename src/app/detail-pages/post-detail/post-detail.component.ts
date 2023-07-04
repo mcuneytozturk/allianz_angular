9cb33d1743b7304369ffd3d7bddf437d210bdc22
Muhsin Cüneyt ÖZTÜRK <mcuneytozturk@gmail.com>
2023-07-05 00:16:33 +0300

    deetail başlangıcı atıldı

--- src/app/detail-pages/post-detail/post-detail.component.ts
+++ src/app/detail-pages/post-detail/post-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Post } from 'src/app/data/Post';
+import { Comment } from 'src/app/data/Comment';
+import { CommentService } from 'src/app/services/comment.service';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-post-detail',
@@ -7,5 +11,17 @@ import { Post } from 'src/app/data/Post';
   styleUrls: ['./post-detail.component.css']
 })
 export class PostDetailComponent {
- @Input() post!: Post;
+  posts: Post[] = [];
+  comments: Comment[] = [];
+  id: number = 0;
+  post?: Post;
+
+  constructor(private postService: PostService, private route: ActivatedRoute, private commentService: CommentService) {
+    const queryParams = this.route.snapshot.queryParams;
+    console.log(queryParams);
+    if(queryParams['id'] !== undefined){
+      this.id = parseInt(queryParams['id'])
+      this.post = this.posts[this.id - 1]
+    }
+  }
 }
