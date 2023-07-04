7ca5d2459fa3d8e5726d6fdd00585dc0666c500b
Muhsin Cüneyt ÖZTÜRK <mcuneytozturk@gmail.com>
2023-07-04 16:54:54 +0300

    addpost eklendi

--- src/app/form-pages/add-post/add-post.component.ts
+++ src/app/form-pages/add-post/add-post.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Post } from 'src/app/data/Post';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-add-post',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-post.component.css']
 })
 export class AddPostComponent {
+ newPost: Post = {
+  postId: 0,
+  userId: 0,
+  categoryId: 0,
+  title: '',
+  content: '',
+  creationDate: '',
+  isPublished: true
+ }
 
+ constructor(private postService: PostService) {
+  this.postService.getPosts().subscribe((posts: Post[]) => {
+    this.newPost.postId = posts.length + 1;
+  });
+}
+
+addNewPost(): void {
+  this.newPost.creationDate = new Date().toISOString().slice(0, 10);
+  this.postService.addPost(this.newPost);
+  this.newPost = {
+    postId: 0,
+    userId: 0,
+    categoryId: 0,
+    title: '',
+    content: '',
+    creationDate: '',
+    isPublished: true
+   }
+}
 }
