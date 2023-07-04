94615f898889e33405c9ac765f600a27655baadf
Muhsin Cüneyt ÖZTÜRK <mcuneytozturk@gmail.com>
2023-07-04 16:05:56 +0300

    commit 2

--- src/app/form-pages/add-user/add-user.component.ts
+++ src/app/form-pages/add-user/add-user.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
-
+import { User } from 'src/app/data/User';
+import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
-  styleUrls: ['./add-user.component.css']
+  styleUrls: ['./add-user.component.css'],
 })
 export class AddUserComponent {
+  newUser: User = {
+    userId: 0,
+    userName: '',
+    email: '',
+    creation_date: new Date().toISOString().slice(0, 10),
+    isActive: true,
+  };
 
+  constructor(private userService: UserService) {
+    this.userService.getUsers().subscribe((users: User[]) => {
+      this.newUser.userId = users.length + 1;
+    });
+  }
+
+  addNewUser(): void {
+    this.newUser.creation_date = new Date().toISOString().slice(0, 10);
+    this.userService.addUser(this.newUser);
+    this.newUser = {
+      userId: 0,
+      userName: '',
+      email: '',
+      creation_date: new Date().toISOString().slice(0, 10),
+      isActive: true,
+    };
+  }
+  
 }
--- src/app/form-pages/form-pages.module.ts
+++ src/app/form-pages/form-pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AddUserComponent } from './add-user/add-user.component';
 import { AddCategoryComponent } from './add-category/add-category.component';
 import { AddPostComponent } from './add-post/add-post.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -13,7 +14,8 @@ import { AddPostComponent } from './add-post/add-post.component';
     AddPostComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     AddUserComponent,
